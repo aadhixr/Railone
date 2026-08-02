a8926e9a2d78351e9eb81aa63c9027829fabb2ab
Aadhi <Aadhi000@users.noreply.github.com>
2026-08-02 23:55:55 +0530

    chore: bump version to 1.9.3

--- app/build/generated/source/buildConfig/debug/app/railonexr/android/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/app/railonexr/android/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "app.railonexr.android";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 92;
-  public static final String VERSION_NAME = "1.9.2";
+  public static final int VERSION_CODE = 93;
+  public static final String VERSION_NAME = "1.9.3";
 }
