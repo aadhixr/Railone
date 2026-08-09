e2c53dff81bf602c811c13b67bc2416972c02017
Aadhi <Aadhi000@users.noreply.github.com>
2026-08-09 20:23:11 +0530

    feat: fine-tune home screen UI and bump version to 2.5.6

--- app/build/generated/source/buildConfig/debug/app/railonexr/android/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/app/railonexr/android/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "app.railonexr.android";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 152;
-  public static final String VERSION_NAME = "2.5.5";
+  public static final int VERSION_CODE = 153;
+  public static final String VERSION_NAME = "2.5.6";
 }
